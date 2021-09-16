4638920d0c647da0d1343e696f9aed72415e3a37
datoyacx <datoyacx@outlook.jp>
2021-09-17 04:18:18 +0700

    update config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,30 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
 	kit: {
 		adapter: adapter({
-			// default options are shown
 			pages: 'build',
 			assets: 'build',
 			fallback: null
-		})
+		}),
+		files: {
+			assets: 'static',
+			routes: 'src/routes',
+			template: 'src/app.html'
+		},
+		floc: false,
+		hydrate: true,
+		router: true,
+		target: '#svelte',
+		ssr: false,
+		trailingSlash: 'never'
 	}
 };
+
+export default config;
